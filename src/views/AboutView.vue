721b995cccbb93ef1fa158f4c24074724a4aa247
christian <cpchristiandelmonte@gmail.com>
2022-04-01 19:33:05 +0800

    added contents for about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,91 @@
 <template>
-  <div class="about">
-    <h1>About</h1>
-  </div>
+  <h1><code>About</code></h1>
+  <main>
+    <section class="icons">
+      <div class="icon-wrapper">
+        <i class="fa-brands fa-vuejs"></i>
+        <span>Vue</span>
+      </div>
+
+      <div class="icon-wrapper">
+        <i class="fa-brands fa-node-js"></i>
+        <span>Node</span>
+      </div>
+
+      <div class="icon-wrapper">
+        <i class="fa-brands fa-js"></i>
+        <span>JavaScript</span>
+      </div>
+
+      <div class="icon-wrapper">
+        <img src="@/assets/images/mysql.png" class="mysql" alt="">
+        <span>MySQL</span>
+      </div>
+      
+    </section>
+
+    <article>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cumque culpa et alias earum quis eum deleniti quod aliquam necessitatibus expedita deserunt sit perspiciatis nostrum laborum excepturi ducimus ex ipsam delectus, veniam debitis commodi dicta vitae. Voluptates temporibus deleniti quis, alias atque aliquam dolore in laborum accusamus ratione illum, minus odit beatae! Blanditiis officiis facilis reprehenderit rerum quos eaque veritatis velit voluptatum nam vel, modi necessitatibus at a aspernatur quas sed ipsam commodi quaerat dicta enim? Nam cumque facere explicabo quaerat modi, doloribus officia eaque, itaque, tenetur mollitia laborum laboriosam enim id quis quo provident illo. Neque beatae quod optio.</p>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit repellat consequatur enim quaerat minus, nemo facere exercitationem architecto quisquam. Maiores ipsum, facere magni assumenda dignissimos quisquam temporibus minima neque odio perspiciatis illum incidunt, cumque ad odit sequi velit dolorem, dolor soluta placeat hic amet provident. Neque eius porro consequatur sed.</p>
+    </article>
+  </main>
 </template>
+
+<script>
+export default {
+  name: 'AboutView'
+}
+</script>
+
+<style scoped>
+  main {
+    display: flex;
+    gap: 1.5rem;
+  }
+
+  .icons {
+    display: flex;
+    gap: 0 1rem;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-content: flex-start;
+    width: 400px;
+  }
+
+  i {
+    font-size: 30px;
+  }
+
+  p {
+    font-size: 17px;
+    font-family:Arial, Helvetica, sans-serif;
+    line-height: 1.7rem;
+  }
+
+  article {
+    width: 60%;
+  }
+
+  .icon-wrapper {
+    margin-top: 1rem;
+    height: 100px;
+    width: 110px;
+    background: #bfd8c0;
+    display: grid;
+    text-align: center;
+    align-content: center;
+    border-radius: 6px;
+    gap: .5rem;
+  }
+
+  span {
+    font-size: 14px;
+    font-family:Arial, Helvetica, sans-serif;
+  }
+
+  .mysql {
+    max-width: 30px;
+    margin: auto;
+  }
+</style>
