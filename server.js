d8b92b90910fe128fa9f364fb1a821ec58369ceb
christian <cpchristiandelmonte@gmail.com>
2022-04-14 12:13:20 +0800

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('projects.json')
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults({ static: "./build" })
 const port = process.env.PORT || 5000
 
 server.use(middlewares)
